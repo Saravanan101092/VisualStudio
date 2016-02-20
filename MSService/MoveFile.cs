00905be906994c596b7b21f51eebf74945f41b4e
saravanan101092 <saravanan101092@gmail.com>
2016-02-20 13:29:48 +0530

    MS service with keylogging and storing in file

---
+++ MSService/MoveFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+{
+    class MoveFile
+    {
+        public static void Main() {
+            Console.WriteLine("moving file");
+            
+        }
+    }
+}
